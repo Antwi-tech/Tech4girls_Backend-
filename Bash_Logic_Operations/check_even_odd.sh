a0f609ad5e9c98246d6f13b08bcdaab7efbad7c5
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2024-10-20 09:19:53 +0000

    Modified even odd and added checking condition

--- Bash_Logic_Operations/check_even_odd.sh
+++ Bash_Logic_Operations/check_even_odd.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+
+if [ $(($1%2)) -eq 0 ]; then
+    echo " The number $1 is even"
+ else 
+   echo "The number $1 is odd"
+ fi
+  
+
+#OR
+
 echo "Enter a number to check is it is even or odd:"
 
 
@@ -8,16 +19,9 @@ if [ $(($num % 2)) -eq 0 ]; then
   echo "The number $num is even"
 else 
   echo "The number $num is odd"
-fi     
-
-#OR
-
+fi   
  
- if [ $(($1%2)) -eq 0 ]; then
-    echo " The number $1 is even"
- else 
-   echo "The number $1 is odd"
- fi       
+        
 
 #NB: when executing the file, you have to enter a positional number then when executed the 
 #user will have to enter another number
