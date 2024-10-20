edc089a772b5840febd56101fd561bbd6697caa2
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2024-10-20 10:21:02 +0000

    Calsulations of numbers

---
+++ Bash_Logic_Operations/calculate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Arithmetic Calculation using arguments
+
+Sum=$(($1+$2))
+Product=$(($1*$2*$3))
+
+echo "The Sum of $1 and $2 is $Sum"
+echo "The product of $1, $2 and $3 is $Product"
