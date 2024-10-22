40e7c81630191792beee73a8a6582f598d45c480
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2024-10-22 17:27:37 +0000

    String expression with arithmetic

---
+++ Python_Basics/f_string_expressions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+#Variables and their respective values
+
+a=10
+b=20
+
+sum=(a+b)
+difference=(a-b)
+product=(a*b)
+
+print(f"The sum ,diffence and product of {a} and {b} are: {sum}, {difference} and  {product}, respectively")
