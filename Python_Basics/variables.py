2d9bb57f1a3c68f44431aea9e652072283fc71db
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2024-10-22 17:14:11 +0000

    Created file and first scritp

---
+++ Python_Basics/variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+#Cities and their values
+
+name='Nana Afua Antwiwaa'
+age=19
+city='Accra'
+
+#Using fstring
+
+print(f"Hello my name is {name}. I am {age} years old and I live in {city}")
