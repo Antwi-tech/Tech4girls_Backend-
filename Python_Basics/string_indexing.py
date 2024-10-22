156d61cd34808e8023bea52e783c1a3fb1652598
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2024-10-22 17:58:00 +0000

    String Indexing

---
+++ Python_Basics/string_indexing.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+#String Sentence
+sentence="Python is powerful and easy to learn"
+
+#print function to print the count of the sentence
+print(len(sentence))
+
+#To print the first letter "P"
+print(sentence[0])
+
+#To print the last alphabet "n"
+print(sentence[35])
+
+#To print the word "powerful"
+print(sentence[10:19])
