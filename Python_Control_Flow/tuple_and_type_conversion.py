a475d4ec74a448d74ec9db35b719e513ed2c926d
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2024-10-27 12:39:04 +0000

    indexing and methods

---
+++ Python_Control_Flow/tuple_and_type_conversion.py
@@ -0,0 +1,28 @@
+#TUPLE
+
+tuple_representation = ( 12, 'Ama', 88 ,'Kwadwo',65)
+#first and last number
+print(tuple_representation[0])
+print(tuple_representation[4])
+
+#count
+numbers= tuple_representation.count(12)
+print(numbers)
+
+#index
+print(tuple_representation.index('Kwadwo'))
+
+#TYPECASTING
+
+num = 56
+
+print(float(num))
+print(int(num))
+print(str(num))
+
+#LIST STRING
+
+names = ['Adjoa','Abena','Jojo','Kofi','Janice']
+joint= ",".join(names)
+print(joint)
+
