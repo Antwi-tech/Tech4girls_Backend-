4b6065e1d3199457b84917198437977835c72f4c
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2024-11-10 12:03:02 +0000

    Error Handling for non-numeric input

--- Error_Handling_Assignment/question5.py
+++ Error_Handling_Assignment/question5.py
@@ -1,9 +1,19 @@
 # Question 6: Age Validator
-age = int(input("Enter your age: "))
-if age < 0:
-    print("Age cannot be negative!")
-elif age > 120:
-    print("That age is unlikely!")
-else:
-    print("Your age is:", age)
+
+while True:
+        try:
+            age = int(input("Enter your age: "))
+        except ValueError as v:
+            print(f'{v}.It not accepted.Enter an integer')
+            continue
+        except Exception as e:
+            print(f'{e}. Invalid')
+            continue
+        if age < 0:
+            print("Age cannot be negative!")
+        elif age > 120:
+            print("That age is unlikely!")
+        else:
+            print("Your age is:", age)
+        break    
 
