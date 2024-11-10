5fe914a1866a974b69acb75bae071555bcdcdd6c
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2024-11-10 11:56:45 +0000

    Added file

---
+++ Error_Handling_Assignment/question5.py
@@ -0,0 +1,9 @@
+# Question 6: Age Validator
+age = int(input("Enter your age: "))
+if age < 0:
+    print("Age cannot be negative!")
+elif age > 120:
+    print("That age is unlikely!")
+else:
+    print("Your age is:", age)
+
